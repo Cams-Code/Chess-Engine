b9ef60a4ae4cc670940dc4821f7ab81ba2992e12
Cameron <cameron.parker@skuuudle.com>
2024-02-24 12:21:46 +0000

    Initial Commit

---
+++ ChessEngine.py
@@ -0,0 +1,138 @@
+"""
+    This class is responsible for storing all information about the current state of the chess game. It will also be responsible for 
+    determining the valid moves at the current state and keeping a log of previous moves.
+"""
+
+class GameState():
+
+    def __init__(self) -> None:
+        # The board is an 8x8 2d list. Each element is 2 characters.
+        # The first character represents the colour of the piece: 'b' or 'w'
+        # The second character represents the type of piece, 'K', 'Q', 'R', 'B', 'N' or 'P'
+        # Empty squares on the chess
+        self.board = [
+            ["bR","bN","bB","bQ","bK","bB","bN","bR"],
+            ["bP" for i in range(8)],
+            ["" for i in range(8)],
+            ["" for i in range(8)],
+            ["" for i in range(8)],
+            ["" for i in range(8)],
+            ["wP" for i in range(8)],
+            ["wR","wN","wB","wQ","wK","wB","wN","wR"],
+        ]
+        self.whiteToMove = True
+        self.moveLog = []
+
+    def wP(self,selected_piece):
+        mult = -1
+        legal_moves = self.move_pawn(selected_piece,mult)
+        return legal_moves
+        
+    def bP(self,selected_piece):
+        mult = 1
+        legal_moves = self.move_pawn(selected_piece,mult)
+        return legal_moves
+    
+    def move_pawn(self,selected_piece:tuple,mult:int):
+        """
+        Pawn move logic:
+            1. Move one square forward if vacant
+            2. If first move, they can move 1 or 2 squares forward if vacant
+            3. Pawns can only capture on the first diagonals infront of them.
+
+            4. Able to capture by en-passant (When an opposing pawn) moves 2 squares past them.
+            5. Promote to any piece when they reach opposing side of the board
+        
+        :params:
+        -- selected piece: tuple. contained the piece name, and x/y co-ords
+        -- mult: int. contains a multiplier for which way the pawn should move. 
+            i.e. white pawn the y co-ord decreases whereas black pawn it increases
+        """
+        legal_moves = []
+        # Implementation of point 1
+        if not self.board[selected_piece[2]+(1*mult)][selected_piece[1]]:
+            legal_moves.append((selected_piece[1], selected_piece[2]+(1*mult)))
+        # Implementation of point 2
+        if (selected_piece[0][0] == 'b' and selected_piece[2] == 1) or (selected_piece[0][0] == 'w' and selected_piece[2] == 6):
+            if not self.board[selected_piece[2]+(2*mult)][selected_piece[1]]:
+                legal_moves.append((selected_piece[1], selected_piece[2]+(2*mult)))
+
+        # Implementation of point 3
+        diagonals = [[selected_piece[1]-1,selected_piece[2]+(1*mult)],[selected_piece[1]+1,selected_piece[2]+(1*mult)]]
+        for diag in diagonals:
+            piece = self.board[diag[1]][diag[0]]
+            if piece and piece[0] != selected_piece[0][0]:
+                legal_moves.append((diag[0],diag[1]))
+
+        return legal_moves
+
+    def wN(self,selected_piece):
+        legal_moves = self.move_knight(selected_piece)
+        return legal_moves
+
+    def bN(self,selected_piece):
+        legal_moves = self.move_knight(selected_piece)
+        return legal_moves
+    
+    def move_knight(self,selected_piece):
+        """
+            Knights move in an L shape in all directions.
+        """
+        legal_moves = []
+        move_sets = [(-1,-2),(1,-2),(-1,2),(1,2),(2,1),(-2,-1),(2,-1),(-2,1)]
+        for move in move_sets:
+            new_x = selected_piece[1] + move[0]
+            new_y = selected_piece[2] + move[1]
+            if (-1 < new_x < 8) and (-1 < new_y < 8):
+                piece_on_sq = self.board[new_y][new_x]
+                if piece_on_sq:
+                    if piece_on_sq[0]!=selected_piece[0][0]:
+                        legal_moves.append((new_x,new_y))
+                else:
+                    legal_moves.append((new_x,new_y))
+        return legal_moves
+
+    def wB(self,selected_piece):
+        legal_moves = self.bishop_move(selected_piece)
+        return legal_moves
+
+    def bB(self,selected_piece):
+        legal_moves = self.bishop_move(selected_piece)
+        return legal_moves
+    
+    def bishop_move(self,selected_piece):
+        """
+            Bishop move logic:
+                1. Bishops can move in any diagonal, on the colour they are on.
+        """
+        legal_moves = []
+
+        range_logic = [(1,8,1),(1,8,-1)]
+        # Diagonal logic needs to be split in 2 - this is the loop for x+y increasing/decreasing
+        for r_set in range_logic:
+            for i in range(r_set[0],r_set[1]):
+                new_x = selected_piece[1] + (i*r_set[2])
+                new_y = selected_piece[2] + (i*r_set[2])
+                if (-1 < new_x < 8) and (-1 < new_y < 8):
+                    # If there is a piece at the x,y then the bishop can either take, or it is blocked for the rest of that diagonal
+                    if self.board[new_y][new_x] != '':
+                        if self.board[new_y][new_x][0] != selected_piece[0][0]:
+                            legal_moves.append((new_x,new_y))
+                        break
+                    else:
+                        legal_moves.append((new_x,new_y))
+
+        # This is the loop for x increasing/ y decreasing and vice versa
+        for r_set in range_logic:
+            for i in range(r_set[0],r_set[1]):
+                new_x = selected_piece[1] + (i*r_set[2])
+                new_y = selected_piece[2] - (i*r_set[2])
+                if (-1 < new_x < 8) and (-1 < new_y < 8):
+                    if self.board[new_y][new_x] != '':
+                        if self.board[new_y][new_x][0] != selected_piece[0][0]:
+                            legal_moves.append((new_x,new_y))
+                        break
+                    else:
+                        legal_moves.append((new_x,new_y))
+
+        return legal_moves
---
+++ ChessMain.py
@@ -0,0 +1,138 @@
+"""
+    This is the main file. It is responsible for handling user inputs and displaying the current GameState object
+"""
+
+import pygame as p
+import ChessEngine
+
+WIDTH = HEIGHT = 512
+DIMENSION = 8
+SQ_SIZE = HEIGHT // DIMENSION
+CIRC_RADIUS = 15
+OCC_CIRC_RADIUS = 25
+OCC_CIRC_WIDTH = 5
+MAX_FPS = 120 # Use for animations later on
+IMAGES = {}
+
+"""
+Initialise global dictionary of images.
+"""
+def loadImages():
+    pieces = ['wP','wN','wB','wR','wQ','wK','bP','bN','bB','bR','bQ','bK']
+    for piece in pieces:
+        IMAGES[piece] = p.transform.scale(p.image.load(f'sprites/{piece}.png'), (SQ_SIZE, SQ_SIZE))
+
+"""
+The main driver for our code. This will handle user inputs and update graphics
+"""
+
+def get_square_under_mouse(board):
+    mouse_pos = p.Vector2(p.mouse.get_pos())
+    x,y = [int(v) // SQ_SIZE for v in mouse_pos]
+
+    try:
+        if x>=0 and y>=0: return (board[y][x], x, y)
+    except IndexError:
+        pass
+    # using x, y co-ords find out the square it is in
+    return None, None, None
+
+def drag(screen, board, selected_piece):
+    if selected_piece and selected_piece[0]:
+        piece, x, y = get_square_under_mouse(board)
+        s1 = p.transform.scale(p.image.load(f'sprites/{selected_piece[0]}.png'), (SQ_SIZE, SQ_SIZE))
+        pos = p.Vector2(p.mouse.get_pos())
+        screen.blit(s1, s1.get_rect(center=pos))
+        return (x, y)
+
+def format_legal_squares(screen,legal_moves):
+    for move_set in legal_moves:
+        x,y = move_set[0],move_set[1]
+        p.draw.circle(screen, (200,0,0), (x*SQ_SIZE, y*SQ_SIZE), CIRC_RADIUS)
+        p.display.update()
+
+def main():
+    p.init()
+    screen = p.display.set_mode([HEIGHT,WIDTH])
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    p.display.set_caption("Chess Engine")
+    gs = ChessEngine.GameState()
+    loadImages()
+
+    selected_piece = None
+    drop_pos = None
+    running = True
+    legal_squares = []
+    while running:
+        piece, x, y = get_square_under_mouse(gs.board)
+        for event in p.event.get():
+            if event.type == p.QUIT:
+                running = False
+            if event.type ==  p.MOUSEBUTTONDOWN:
+                selected_piece = piece, x, y
+                if selected_piece[0]:
+                    # Move calculations are labelled the same as pieces (e.g. wP move logic is function wP)
+                    func_name = getattr(gs, selected_piece[0])
+                    legal_squares = func_name(selected_piece)
+                else:
+                    legal_squares = []
+                # format_legal_squares(screen,legal_squares)
+                if not drop_pos:
+                    og_x, og_y = x, y
+                gs.board[y][x] = ''
+            if event.type == p.MOUSEBUTTONUP:
+                if drop_pos:
+                    # Unable to move to that position as not in chessboard
+                    if drop_pos[0]==None or drop_pos not in legal_squares:
+                        gs.board[og_y][og_x] = selected_piece[0]
+                        selected_piece = None
+                        drop_pos = None
+                        
+                    else:
+                        # Remove piece from old position on board once moved
+
+                        piece, old_x, old_y = selected_piece
+                        gs.board[old_y][old_x] = ''
+                        new_x, new_y = drop_pos
+                        gs.board[new_y][new_x] = piece
+                        
+                legal_squares = []
+                selected_piece = None
+                drop_pos = None
+
+        drawGameState(screen, gs)
+        # format_legal_squares(screen,legal_squares)
+        drop_pos = drag(screen, gs.board, selected_piece)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+
+def drawGameState(screen, gs):
+    drawBoard(screen) # draw squares on the board
+    drawPieces(screen, gs.board) # draw pieces on the squares
+
+"""
+    Draw the squares on the board
+"""
+
+def drawBoard(screen):
+    colors = [p.Color("white"), p.Color("gray")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[((r+c) % 2)]
+            p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+            # Note: p.Rect args are: x co-ord to begin. y co-ord to begin. size of x axis to draw. size of y axis to draw
+
+"""
+    Draw the pieces on the board using current GameState
+"""
+
+def drawPieces(screen, board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece: # Not an empty square
+                screen.blit(IMAGES[piece], p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+if __name__ == '__main__':
+    main()
